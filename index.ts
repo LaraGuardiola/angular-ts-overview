fc567fc8a9f9f2429072759c62f37fbc4f62a16d
Sergio <webdev.slara@gmail.com>
2022-05-13 13:29:18 +0200

    tested type annotations and type inference

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-console.log('hi there')
+//const myName: string = 'Sergio'
 
-const myName: string = 'Sergio'
+//: string is the TYPE ANNOTATION, there are many types:
+
+/*
+string / number / boolean / undefined / null / any (basic ones)
+*/
+
+/*let myName = 'Stephen'
+
+myName = 10*/
+
+//this case is TYPE INFERENCE, even if its a let in normal js, its already initialized as a string, cannot change to number
+//if it was undeclared, it would be default type any
